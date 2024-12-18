09f89c75d7d1246c5c28de042dee3548f39e1076
Jihad <Jihad.alaswa@d-n.be>
2024-12-18 21:55:59 +0100

    add increase/decrease function to calculate the wine price

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -17,36 +17,64 @@ private const val MIN_PRICE = 0
 private const val ECO_BRILLIANT_WINE = "Eco Brilliant Wine"
 
 class WineShop(var items: List<Wine>) {
+
+    //check if the wine is standard wine
+    private fun isStandardWine(wine: Wine): Boolean {
+        return wine.name != BOURDEAUX_CONSERVATO &&
+                wine.name != BOURGOGNE_CONSERVATO &&
+                !wine.name.startsWith(EVENT)
+    }
+
+    //check if wine brewed by Alexander the Great
+    private fun isNotBrewedByAlexander(wine: Wine): Boolean {
+        return wine.name != WINE_BREWED_BY_ALEXANDER_THE_GREAT
+    }
+
+
+    //decrease the wine price by 1 if greater than min price
+    private fun decreaseWinePrice(wine: Wine) {
+        if (wine.price > 0) {
+            if (isNotBrewedByAlexander(wine)) {
+                wine.price -= 1
+            }
+        }
+    }
+
+    /**
+     * increase the price of wine by given value (default is 1)
+     * the price not exceed the maximum allowed price
+     */
+    private fun increasePriceIfPossible(wine: Wine, by: Int = 1) {
+        if (wine.price < MAX_PRICE) {
+            wine.price += by
+        }
+    }
+
+
     fun next() {
         // Wine Shop logic
         for (i in items.indices) {
-            if (items[i].name != BOURDEAUX_CONSERVATO && items[i].name != BOURGOGNE_CONSERVATO && !items[i].name.startsWith(EVENT)) {
-                if (items[i].price > 0) {
-                    if (items[i].name != WINE_BREWED_BY_ALEXANDER_THE_GREAT) {
-                        items[i].price = items[i].price - 1
-                    }
-                }
+            if (isStandardWine(items[i])) {
+                decreaseWinePrice(items[i])
             } else {
                 if (items[i].price < 100) {
                     items[i].price = items[i].price + 1
 
                     if (items[i].name.startsWith(EVENT)) {
                         if (items[i].expiresInYears < 8) {
-                            if (items[i].price < 100) {
-                                items[i].price = items[i].price + 1
-                            }
+                            increasePriceIfPossible(items[i])
                         }
 
                         if (items[i].expiresInYears < 3) {
-                            if (items[i].price < 100) {
-                                items[i].price = items[i].price + 2
-                            }
+
+                            increasePriceIfPossible(items[i], 2)
+
                         }
                     }
                 }
             }
 
-            if (items[i].name != WINE_BREWED_BY_ALEXANDER_THE_GREAT) {
+            if (isNotBrewedByAlexander(items[i])) {
                 items[i].expiresInYears = items[i].expiresInYears - 1
             } else if (items[i].price < 0) {
                 items[i].price = 0
@@ -55,17 +83,13 @@ class WineShop(var items: List<Wine>) {
             if (items[i].expiresInYears < 0) {
                 if (!items[i].name.contains(CONSERVATO)) {
                     if (!items[i].name.contains(EVENT)) {
-                        if (items[i].price > 0) {
-                            if (items[i].name != WINE_BREWED_BY_ALEXANDER_THE_GREAT) {
-                                items[i].price = items[i].price - 1
-                            }
-                        }
+                        decreaseWinePrice(items[i])
                     } else {
                         items[i].price = items[i].price - items[i].price
                     }
                 } else {
                     if (items[i].price < 100) {
-                        items[i].price = items[i].price + 1
+                        increasePriceIfPossible(items[i])
                     }
                 }
             }
@@ -75,4 +99,6 @@ class WineShop(var items: List<Wine>) {
             }
         }
     }
+
+
 }
